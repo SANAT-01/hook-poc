c8eda81fa2ca7cf12c48acea000f688cd2a92dee
Sanant Tudu <Sanat.Tudu@winjittechnologiespvtltd.local>
2025-03-22 23:00:46 +0530

    Revert "UI changes with added personal page with CSR"
    This reverts commit ba65835de0e4be43f7d6986b37c7e419f67a4b54.

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["dev.media.hookmusic.com", "images.unsplash.com"],
-  },
+  /* config options here */
 };
 
 export default nextConfig;
